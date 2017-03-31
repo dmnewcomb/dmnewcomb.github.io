ef96efc6dd06ca77cc62a4c1f7cfa36e2e5ff0eb
dmnewcomb <dnewcomb23@gmail.com>
2017-03-31 10:01:21 -0700

    allow choosing conference

--- js/main.js
+++ js/main.js
@@ -22,17 +22,76 @@ $('.home.index').ready(function() {
     }
   });
 
-  $('#drop_e').fastselect();
-  $('#drop_w').fastselect();
+  $('#drop_lc').fastselect();
+  $('#drop_rc').fastselect();
+  $('#drop_le').fastselect();
+  $('#drop_lw').fastselect();
+  $('#drop_re').fastselect();
+  $('#drop_rw').fastselect();
+
+
+  $("#drop_lc, #drop_rc").on("change", function(e) {
+    // NProgress.start();
+    var chosen_text = $(this).val();
+    var side = "l"
+    if ($(this)[0].id === "drop_rc") side = "r"
+    console.log(side)
+    var container = "";
+    var img_container = "#zoom_img_"+side;
+
+    if(chosen_text === "Eastern Conference") {
+      container = "drop-container_"+side+"w";
+      var hide = document.getElementById(container);
+      if (hide.style.display !== 'none') {
+          hide.style.display = 'none';
+      }
+
+      container = "drop-container_"+side+"e";
+      var show = document.getElementById(container);
+      console.log(show)
+      if (show.style.display === 'none') {
+          show.style.display = 'block';
+      }
+      img_url_container = "drop_"+side+"e";
+      img_url_div = GetElementInsideContainer(container, img_url_container)
+      console.log(img_url_div)
+      default_text = $( "#drop_"+side+"e option:selected" ).text();
+      img_url = "images/" + default_text + ".png";
+      console.log(img_url)
+      $(img_container)[0].setAttribute('src', img_url);
+    }
+
+    if(chosen_text === "Western Conference") {
+      container = "drop-container_"+side+"e";
+      var hide = document.getElementById(container);
+      if (hide.style.display !== 'none') {
+          hide.style.display = 'none';
+      }
+
+      container = "drop-container_"+side+"w";
+      var show = document.getElementById(container);
+      if (show.style.display === 'none') {
+          show.style.display = 'block';
+      }
+      img_url_container = "drop_"+side+"w";
+      img_url_div = GetElementInsideContainer(container, img_url_container)
+      console.log(img_url_div)
+      default_text = $( "#drop_"+side+"w option:selected" ).text();
+      img_url = "images/" + default_text + ".png";
+      console.log(img_url)
+      $(img_container)[0].setAttribute('src', img_url);
+    }
+  });
 
   // function that replaces the image with the correct one
   // for the NBA project
-  $("#drop_e, #drop_w").on("change", function(e) {
+  $("#drop_le, #drop_lw, #drop_re, #drop_rw").on("change", function(e) {
     NProgress.start();
     var chosen_text = $(this).val();
+    console.log(chosen_text)
     var image_url = "images/" + chosen_text + ".png";
-    var img_container = "#zoom_img_w";
-    if($(this)[0].id === "drop_e") img_container = "#zoom_img_e";
+    var img_container = "#zoom_img_r";
+    if($(this)[0].id === "drop_le" || $(this)[0].id === "drop_lw") img_container = "#zoom_img_l";
 
     // change the image to the correct one
     $(img_container)[0].setAttribute('src', image_url);
@@ -42,6 +101,19 @@ $('.home.index').ready(function() {
     }
     window.setTimeout(waitFunction, 2000);
   });
+
+  function GetElementInsideContainer(containerID, childID) {
+    var elm = {};
+    var elms = document.getElementById(containerID).getElementsByTagName("*");
+    console.log(elms)
+    for (var i = 0; i < elms.length; i++) {
+        if (elms[i].id === childID) {
+            elm = elms[i];
+            break;
+        }
+    }
+    return elm;
+  }
 
   // zoom functions
   var zoom_active = true;
@@ -68,8 +140,10 @@ $('.home.index').ready(function() {
     if($('.navbar').width() > 767) {
       // Add zoom containers to DOM
       zoom_active = true;
-      addZoom("#zoom_img_e");
-      addZoom("#zoom_img_w");
+      addZoom("#zoom_img_le");
+      addZoom("#zoom_img_lw");
+      addZoom("#zoom_img_re");
+      addZoom("#zoom_img_rw");
     }  
   };
 
